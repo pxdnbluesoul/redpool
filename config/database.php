6e39065295da4fd6e4ab65d2f1c6bf47bccccf2b
bluesoul <github@nags.me>
2019-02-01 14:29:40 -0700

    Leaked database creds reset and moved to .env

--- config/database.php
+++ config/database.php
@@ -55,10 +55,10 @@ return array(
 
                 'mysql' => array(
                         'driver'    => 'mysql',
-                        'host'      => 'localhost',
-                        'database'  => 'tbjevszgcp',
-                        'username'  => 'tbjevszgcp',
-                        'password'  => 'RT9RhT4dvX',
+                        'host'      => env('DB_HOST'),
+                        'database'  => env('DB_DATABASE'),
+                        'username'  => env('DB_USERNAME'),
+                        'password'  => env('DB_PASSWORD'),
                         'charset'   => 'utf8',
                         'collation' => 'utf8_unicode_ci',
                         'prefix'    => '',
