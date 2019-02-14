8497f7aa92b21391980fd0af5833aef6201334fc
bluesoul <github@nags.me>
2019-02-14 09:04:21 -0700

    Controllers

---
+++ app/Http/Controllers/CritController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Crit;
+use Illuminate\Http\Request;
+
+class CritController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Crit  $crit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Crit $crit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Crit  $crit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Crit $crit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Crit  $crit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Crit $crit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Crit  $crit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Crit $crit)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PasteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Paste;
+use Illuminate\Http\Request;
+
+class PasteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Paste  $paste
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Paste $paste)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Paste  $paste
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Paste $paste)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Paste  $paste
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Paste $paste)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Paste  $paste
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Paste $paste)
+    {
+        //
+    }
+}
