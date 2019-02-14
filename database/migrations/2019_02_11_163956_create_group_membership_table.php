3d42c58075aa9b4c42fa0e22760d3253e15fbfdf
bluesoul <github@nags.me>
2019-02-14 09:03:58 -0700

    Migrations

--- database/migrations/2019_02_11_163956_create_group_membership_table.php
+++ database/migrations/2019_02_11_163956_create_group_membership_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGroupMembershipTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('group_membership', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
-            $table->string('wikidotusername');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->unsignedInteger('group_id');
+            $table->string('member_type');
+            $table->unsignedInteger('member_id');
             $table->json('metadata')->nullable();
-            $table->rememberToken();
             $table->timestamps();
+            $table->unique(['group_id','member_type','member_id']);
         });
     }
 
@@ -33,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('group_membership');
     }
 }
--- database/migrations/2019_02_11_180432_create_crits_table.php
+++ database/migrations/2019_02_11_180432_create_crits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCritsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('crits', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
-            $table->string('wikidotusername');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->unsignedInteger('user_id');
+            $table->string('name');
+            $table->text('body');
             $table->json('metadata')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('crits');
     }
 }
